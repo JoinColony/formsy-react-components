07aa33c355f8a4d1f9af38f7ecf2eecfa6b94880
Christian Maniewski <chmanie@fastmail.com>
2016-03-18 15:37:38 +0100

    Simplify Input component for bulma

--- src/input.js
+++ src/input.js
@@ -6,7 +6,7 @@ var React = require('react');
 var Formsy = require('formsy-react');
 var ComponentMixin = require('./mixins/component');
 var Row = require('./row');
-var Icon = require('./icon');
+// var Icon = require('./icon');
 
 var Input = React.createClass({
 
@@ -30,26 +30,26 @@ var Input = React.createClass({
             'time',
             'url',
             'week'
-        ]),
-        addonBefore: React.PropTypes.oneOfType([
-            React.PropTypes.string,
-            React.PropTypes.node
-        ]),
-        addonAfter: React.PropTypes.oneOfType([
-            React.PropTypes.string,
-            React.PropTypes.node
-        ]),
-        buttonBefore: React.PropTypes.node,
-        buttonAfter: React.PropTypes.node
+        ])
+        // addonBefore: React.PropTypes.oneOfType([
+        //     React.PropTypes.string,
+        //     React.PropTypes.node
+        // ]),
+        // addonAfter: React.PropTypes.oneOfType([
+        //     React.PropTypes.string,
+        //     React.PropTypes.node
+        // ]),
+        // buttonBefore: React.PropTypes.node,
+        // buttonAfter: React.PropTypes.node
     },
 
     getDefaultProps: function() {
         return {
-            type: 'text',
-            addonBefore: null,
-            addonAfter: null,
-            buttonBefore: null,
-            buttonAfter: null
+            type: 'text'
+            // addonBefore: null,
+            // addonAfter: null,
+            // buttonBefore: null,
+            // buttonAfter: null
         };
     },
 
@@ -66,20 +66,21 @@ var Input = React.createClass({
             return element;
         }
 
-        if (this.props.addonBefore || this.props.addonAfter || this.props.buttonBefore || this.props.buttonAfter) {
-            element = this.renderInputGroup(element);
-        }
+        // if (this.props.addonBefore || this.props.addonAfter || this.props.buttonBefore || this.props.buttonAfter) {
+        //     element = this.renderInputGroup(element);
+        // }
 
         if (this.getLayout() === 'elementOnly') {
             return element;
         }
 
-        var warningIcon = '';
-        if (this.showErrors()) {
-            warningIcon = (
-                <Icon symbol="remove" className="form-control-feedback" />
-            );
-        }
+        // var warningIcon = '';
+        // if (this.showErrors()) {
+        //     warningIcon = (<span>Error</span>);
+        //     // warningIcon = (
+        //     //     <Icon symbol="remove" className="form-control-feedback" />
+        //     // );
+        // }
 
         return (
             <Row
@@ -87,18 +88,18 @@ var Input = React.createClass({
                 htmlFor={this.getId()}
             >
                 {element}
-                {warningIcon}
-                {this.renderHelp()}
                 {this.renderErrorMessage()}
             </Row>
         );
+        // {warningIcon}
+        // {this.renderHelp()}
     },
 
     renderElement: function() {
-        var className = 'form-control';
-        if (['range'].indexOf(this.props.type) !== -1) {
-            className = null;
-        }
+        var className = 'input';
+        // if (['range'].indexOf(this.props.type) !== -1) {
+        //     className = null;
+        // }
         return (
             <input
                 className={className}
@@ -110,37 +111,37 @@ var Input = React.createClass({
                 disabled={this.isFormDisabled() || this.props.disabled}
             />
         );
-    },
-
-    renderInputGroup: function(element) {
-        return (
-            <div className="input-group">
-                {this.renderAddon(this.props.addonBefore)}
-                {this.renderButton(this.props.buttonBefore)}
-                {element}
-                {this.renderAddon(this.props.addonAfter)}
-                {this.renderButton(this.props.buttonAfter)}
-            </div>
-        );
-    },
-
-    renderAddon: function(addon) {
-        if (!addon) {
-            return false;
-        }
-        return (
-            <span className="input-group-addon">{addon}</span>
-        );
-    },
-
-    renderButton: function(button) {
-        if (!button) {
-            return false;
-        }
-        return (
-            <span className="input-group-btn">{button}</span>
-        );
     }
+
+    // renderInputGroup: function(element) {
+    //     return (
+    //         <div className="input-group">
+    //             {this.renderAddon(this.props.addonBefore)}
+    //             {this.renderButton(this.props.buttonBefore)}
+    //             {element}
+    //             {this.renderAddon(this.props.addonAfter)}
+    //             {this.renderButton(this.props.buttonAfter)}
+    //         </div>
+    //     );
+    // },
+
+    // renderAddon: function(addon) {
+    //     if (!addon) {
+    //         return false;
+    //     }
+    //     return (
+    //         <span className="input-group-addon">{addon}</span>
+    //     );
+    // },
+
+    // renderButton: function(button) {
+    //     if (!button) {
+    //         return false;
+    //     }
+    //     return (
+    //         <span className="input-group-btn">{button}</span>
+    //     );
+    // }
 
 });
 
